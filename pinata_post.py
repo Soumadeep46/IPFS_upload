a7f2eba27751cc28470ab224a30a2475fdb4b656
Soumadeep Samanta <161934701+Soumadeep46@users.noreply.github.com>
2024-11-21 17:16:01 +0530

    Add files via upload

---
+++ pinata_post.py
@@ -0,0 +1,21 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def upload_to_pinata(filepath, jwt_token):
+    url ="https://api.pinata.cloud/pinning/pinFileToIPFS"
+    headers={'Authorization': f'Bearer {jwt_token}'}
+
+    with open(filepath,'rb') as file:
+        response= requests.post(url,files={'file':file}, headers=headers)
+        return response.json()
+    
+PINATA_JWT_TOKEN = os.getenv('PINATA_JWT_TOKEN')
+
+
+
+FILE_PATH= 'GOT.jpg'
+
+print(upload_to_pinata(FILE_PATH,PINATA_JWT_TOKEN))
